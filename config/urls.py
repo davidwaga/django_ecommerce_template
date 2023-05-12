847e8de4c7d4a77653b8b27f76104deff3394e0e
davidwaga <davidwaga75@gmail.com>
2023-05-12 18:20:19 +0300

    [added]migrations

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', views.HomeView.as_view(), name='home'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('staff/', include('staff.urls', namespace='staff')),
+    path('profile/', views.ProfileView.as_view(), name='profile')
 ]
